1eea1e8418b3d3ac4cf5ee8f1b089173aef86b23
FelipeEduardo369 <felipeeduardo369@hotmail.com>
2024-06-22 01:59:50 -0300

    Portifolio

---
+++ index.js
@@ -0,0 +1,19 @@
+/*Faltando algumas interações com os emojis e botões, será adicionado*/
+
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+menuHamburguer.addEventListener('click', () => {
+    toggleMenu();
+})
+
+function toggleMenu() {
+    const nav = document.querySelector ('.nav-responsive');
+    console.log(nav);
+    menuHamburguer.classList.toggle('change');
+
+    if (menuHamburguer.classList.contains('change')){
+        nav.style.display = 'block';
+    } else {
+        nav.style.display = 'none';
+    }
+}
+
